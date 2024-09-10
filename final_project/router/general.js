ddca9036517c14f4e8239dba22930b5511909f0f
DESKTOP-0UO9BQF\AnuradhA <h.supun.anuradha@gmail.com>
2024-09-10 11:15:13 +0530

    task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,22 +25,31 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const bookAuthor = req.params.author;
-
+  const regex = new RegExp(bookAuthor, 'gi');
   let filteredBooks = []; 
   Object.values(books).forEach((book) => {
-    
-    if(book.author == bookAuthor) {
-      //console.log(index, book);
+    if(book.author.search(regex) != -1) {
       filteredBooks.push(book);
     }
+    // if(book.author == bookAuthor) {
+    //   //console.log(index, book);
+    //   filteredBooks.push(book);
+    // }
   });
   return res.send(JSON.stringify(filteredBooks, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookTitle = req.params.title;
+  const regex = new RegExp(bookTitle, 'gi');
+  let filteredBooks = []; 
+  Object.values(books).forEach((book) => {
+    if(book.title.search(regex) != -1) {
+      filteredBooks.push(book);
+    }
+  });
+  return res.send(JSON.stringify(filteredBooks, null, 4));
 });
 
 //  Get book review
